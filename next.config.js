71b3cc6c0b1b3674c234eb3b766b4f31efa5b2e3
KyleGamezYT <79540233+KyleGamezYT@users.noreply.github.com>
2025-08-18 01:34:26 +0000

    a

--- next.config.js
+++ next.config.js
@@ -3,47 +3,54 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/discord',
-        destination: 'https://discord.gg/gvro',
+        source: "/discord",
+        destination: "https://discord.gg/gvro",
         permanent: false,
       },
       {
-        source: '/spotify',
-        destination: 'https://open.spotify.com/user/your-spotify-id',
+        source: "/spotify",
+        destination: "https://open.spotify.com/user/your-spotify-id",
         permanent: false,
       },
       {
-        source: '/tiktok',
-        destination: 'https://tiktok.com/@your-tiktok-username',
+        source: "/tiktok",
+        destination: "https://tiktok.com/@your-tiktok-username",
         permanent: false,
       },
       {
-        source: '/cashapp',
-        destination: 'https://cash.app/$your-cashapp-username',
+        source: "/cashapp",
+        destination: "https://cash.app/$your-cashapp-username",
         permanent: false,
       },
       {
-        source: '/email',
-        destination: 'mailto:your-email@example.com',
+        source: "/email",
+        destination: "mailto:your-email@example.com",
         permanent: false,
       },
       {
-        source: '/roblox',
-        destination: 'https://www.roblox.com/user/your-roblox-id',
+        source: "/roblox",
+        destination: "https://www.roblox.com/user/your-roblox-id",
         permanent: false,
       },
       {
-        source: '/instagram',
-        destination: 'https://www.instagram.com/tactilekyle/',
+        source: "/instagram",
+        destination: "https://www.instagram.com/tactilekyle/",
         permanent: false,
-      }, 
+      },
       {
-        source: '/karaoke',
-        destination: 'https://medal.tv/games/roblox/clips/kUU2CeHAjBXbWP2yH?invite=cr-MSxvNGcsNDE0MDc5ODMx&v=133',
+        source: "/karaoke",
+        destination:
+          "https://medal.tv/games/roblox/clips/kUU2CeHAjBXbWP2yH?invite=cr-MSxvNGcsNDE0MDc5ODMx&v=133",
         permanent: false,
-      }, 
-    ]
+      },
+      {
+        source: "/gvro/football",
+        destination:
+          "https://fantasy.espn.com/football/league?leagueId=726011510",
+        permanent: false,
+      },
+    ];
   },
-}
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
